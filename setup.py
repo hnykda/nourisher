c59f879ce1c1db5ffec8bac68b74cf20a7cb9724
kotrfa <kotrfa@gmail.com>
2015-07-21 13:16:19 +0200

    setup.py tunned

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='Nourisher',
-      version='0.9',
+      version='0.9.1',
       description='Package for collecting info about feeds',
       author='Daniel Hnyk',
       author_email='kotrfa@gmail.com',
       url='https://github.com/kotrfa/nourisher',
       install_requires=['feedparser', 'newspaper3k', 'selenium', 'pymongo', 'beautifulsoup4'],
+      packages=["nourisher",],
+      include_package_data=True,
      )
